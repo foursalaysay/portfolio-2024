a2041bf5a963634f52eac8a865b69afd6a0ff537
John Kyle Salaysay <foursalaysay@gmail.com>
2024-01-30 23:10:24 +0800

    homepage

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,32 +1,38 @@
-'use client'
+"use client";
 
-import { motion } from 'framer-motion'
-import Image from "next/image"
+import { motion } from "framer-motion";
+import Image from "next/image";
 
-import logo from '/kyle.svg'
+import logo from "./../../../public/kyle.svg";
 
-export default function HomePage(){
+// COMPONENTS
+import { Navbar } from "@/components/reusables/index";
 
-    const variants = {
-        initial : {
-            x: 0,
-        }, 
-        animate : {
-            x : 500,
-        transition : {
-            type: "spring", stiffness: 100
-        }
-        }
-    }
-    return(
-        <motion.div variants={variants}>
-            <Image
-            src={logo}
-            height={500}
-            width={500}
-            alt="image"
-             />
-            <h1>John Kyle Salaysay</h1>
-        </motion.div>
-    )
+export default function HomePage() {
+  const variants = {
+    initial: {
+      x: 0,
+    },
+    animate: {
+      x: 500,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+      },
+    },
+  };
+  return (
+    <motion.div variants={variants} className="w-full h-screen">
+      <Navbar />
+
+      <Image
+        src={logo}
+        height={500}
+        width={500}
+        className="w-50% rounded border-2"
+        alt="image"
+      />
+      <h1>John Kyle Salaysay</h1>
+    </motion.div>
+  );
 }
